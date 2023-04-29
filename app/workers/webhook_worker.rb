4afb0cf5f43d0080dbd7300620e4dda6f34b4354
Maikon Douglas <maikon.douglas@vindi.com.br>
2023-04-28 21:42:49 -0300

    refactor worker

--- app/workers/webhook_worker.rb
+++ app/workers/webhook_worker.rb
@@ -1,49 +1,39 @@
+# frozen_string_literal: true
+
 require 'http'
 
 class WebhookWorker
   include Sidekiq::Worker
 
-  def perform(webhook_event_id)
-    webhook_event = WebhookEvent.find_by(id: webhook_event_id)
-    return if
-      webhook_event.nil?
+  def perform(event)
+    @webhook_event    = WebhookEvent.find_by(event: event)
+    @webhook_endpoint = @webhook_event&.webhook_endpoint
+    return if @webhook_event.blank? || @webhook_endpoint.blank?
 
-    webhook_endpoint = webhook_event.webhook_endpoint
-    return if
-      webhook_endpoint.nil?
-
-    # Send the webhook request with a 30 second timeout.
-    response = HTTP.timeout(30)
-                   .headers(
-                     'User-Agent' => 'rails_webhook_system/1.0',
-                     'Content-Type' => 'application/json'
-                   )
-                   .post(
-                     webhook_endpoint.url,
-                     body: {
-                       event: webhook_event.event,
-                       payload: webhook_event.payload
-                     }.to_json
-                   )
-
-    # Store the webhook response.
-    webhook_event.update(response: {
-                           headers: response.headers.to_h,
-                           code: response.code.to_i,
-                           body: response.body.to_s
-                         })
-
-    # Raise a failed request error and let Sidekiq handle retrying.
-    raise FailedRequestError unless
-      response.status.success?
+    response = handle_request
+    @webhook_event.update(response: {
+                            headers: response.headers.to_h,
+                            code: response.code.to_i,
+                            body: response.body.to_s
+                          })
   rescue HTTP::TimeoutError
-    # This error means the webhook endpoint timed out. We can either
-    # raise a failed request error to trigger a retry, or leave it
-    # as-is and consider timeouts terminal. We'll do the latter.
-    webhook_event.update(response: { error: 'TIMEOUT_ERROR' })
+    @webhook_event.update(response: { error: 'TIMEOUT_ERROR' })
   end
 
-  # General failed request error that we're going to use to signal
-  # Sidekiq to retry our webhook worker.
-  class FailedRequestError < StandardError; end
+  private
+
+  def handle_request
+    @handle_request ||= HTTP.timeout(30)
+                            .headers(
+                              'User-Agent' => 'rails_webhook_system/1.0',
+                              'Content-Type' => 'application/json'
+                            )
+                            .post(
+                              @webhook_endpoint.url,
+                              body: {
+                                event: @webhook_event.event,
+                                payload: @webhook_event.payload
+                              }.to_json
+                            )
+  end
 end
